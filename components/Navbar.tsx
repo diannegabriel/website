d9b7e7a10c0f87686c4c3560a4d9fda682f85874
Dianne <mdianne.gabriel@gmail.com>
2023-07-14 12:21:08 -0400

    Add styling to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-  <div>Navbar</div>
+  <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyBgColor px-4">Navbar</div>
   )
 }
 
