115913fda0415121772614f1e9488aa83f3ef695
Dianne <mdianne.gabriel@gmail.com>
2023-07-14 12:36:31 -0400

    Fix typo in div classname

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 import { logoGif } from "@/public/assets";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
   <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyBgColor px-4">
-    <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify between">
+    <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between">
       <div>
         <Image className="w-40" src={logoGif} alt="logo" />
       </div>
