882843a96404ef65bafc4985a1e63dc1e7345183
Dianne <mdianne.gabriel@gmail.com>
2023-07-14 12:53:38 -0400

    Add new logo design. Import dianneGif

--- public/assets/index.tsx
+++ public/assets/index.tsx
@@ -1,4 +1,5 @@
 import logoPng from "./images/logo.png";
 import logoGif from "./images/logo.gif";
+import dianneGif from "./images/dianne.gif";
 
-export { logoPng, logoGif };
+export { logoPng, logoGif, dianneGif };
