eb7b55053cd611626c8736da38a9395af129c794
Dianne <mdianne.gabriel@gmail.com>
2023-07-14 12:32:51 -0400

    Resize logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyBgColor px-4">
     <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify between">
       <div>
-        <Image src={logoGif} alt="logo" />
+        <Image className="w-40" src={logoGif} alt="logo" />
       </div>
     </div>
   </div>
