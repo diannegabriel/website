75d03292c882b1978258a8298eee874b2e68280d
Dianne <mdianne.gabriel@gmail.com>
2023-07-14 11:34:59 -0400

    Add Head and title to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import Navbar from '@/components/Navbar'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="w-full h-screen font-bodyFont bg-bodyBgColor text-textLight overflow-x-hidden overflow-y-scroll">
-      <Navbar />
-      Dianne Gabriel - Portfolio Website
-    </main>
+    <>
+      <Head>
+        <title>Dianne's Portfolio</title>
+      </Head>
+      <main className="w-full h-screen font-bodyFont bg-bodyBgColor text-textLight overflow-x-hidden overflow-y-scroll">
+        <Navbar />
+        Dianne Gabriel - Portfolio Website
+      </main>
+    </>
   )
 }
