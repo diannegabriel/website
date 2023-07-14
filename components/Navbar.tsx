7c35cf1be6fe31afc889c0ccd648d339ca2cd538
Dianne <mdianne.gabriel@gmail.com>
2023-07-14 12:57:24 -0400

    Import framer motion to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import { dianneGif } from "@/public/assets";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
