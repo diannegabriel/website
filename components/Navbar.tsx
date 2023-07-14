e0adbce296cd25871f5ed5aa3e971b9df9ecc204
Dianne <mdianne.gabriel@gmail.com>
2023-07-14 12:43:48 -0400

    Switch titleFont to bodyFont

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,13 +6,13 @@ import React from "react";
 const Navbar = () => {
   return (
   <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyBgColor px-4">
-    <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between">
+    <div className="max-w-container h-full mx-auto py-1 font-bodyFont flex items-center justify-between">
       <div>
         <Image className="w-40" src={logoGif} alt="logo" />
       </div>
       <div>
         <ul>
-        <Link href="#home" className="flex items-center gap-1 font-medium text-2xl text-textDark hover:text-textAccent cursor-pointer duration-300 nav-link">
+        <Link href="#home" className="flex items-center gap-1 font-medium text-lg text-textDark hover:text-textAccent cursor-pointer duration-300 nav-link">
           <li>Home</li>
         </Link>
         </ul>
