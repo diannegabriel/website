c75027eff5f03512f132d568eb85e64fd0d7bd78
Dianne <mdianne.gabriel@gmail.com>
2023-07-14 12:28:20 -0400

    Change div font to titleFont

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
   <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyBgColor px-4">
-    <div className="max-w-container h-full mx-auto py-1 font-bodyFont flex items-center justify between">
-
+    <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify between">
     </div>
   </div>
   )
