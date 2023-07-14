f3300fcd8f254cb8ed4f97bb2001a82c3dec9afe
Dianne <mdianne.gabriel@gmail.com>
2023-07-14 12:31:07 -0400

    Add logo to the Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,14 @@
+import { logoGif } from "@/public/assets";
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
   return (
   <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyBgColor px-4">
     <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify between">
+      <div>
+        <Image src={logoGif} alt="logo" />
+      </div>
     </div>
   </div>
   )
