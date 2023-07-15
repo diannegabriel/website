48b950ea66be50d9e104e1741f7e9b7517e5266f
Dianne <mdianne.gabriel@gmail.com>
2023-07-15 13:45:04 -0400

    Set up main content in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Navbar from '@/components/Navbar'
+import Navbar from "@/components/Navbar";
 // import { Inter } from 'next/font/google'
-import Head from 'next/head'
+import Head from "next/head";
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -12,8 +12,8 @@ export default function Home() {
       </Head>
       <main className="w-full h-screen font-bodyFont bg-bodyBgColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
-        Dianne Gabriel - Portfolio Website
+        <div className="w-full h-[88vh] xl:flex- items-center gap-20 justify-between"></div>
       </main>
     </>
-  )
+  );
 }
