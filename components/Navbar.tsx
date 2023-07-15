ecd312ab30b5a6d42f0e8149b07f3cc26ba1c46c
Dianne <mdianne.gabriel@gmail.com>
2023-07-15 13:40:03 -0400

    Customize middle hamburger line

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -76,6 +76,7 @@ const Navbar = () => {
         {/* Hamburger icon */}
         <div className="w-6 h-5 flex flex-col justify-between items-center mdL:hidden text-4xl text-textAccent cursor-pointer overflow-hidden group">
           <span className="w-full h-[2px] bg-textAccent inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
+          <span className="w-full h-[2px] bg-textAccent inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300"></span>
         </div>
       </div>
     </div>
