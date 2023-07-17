68c6a8e9023a272cfede4ff94f760692dda19c34
Dianne <mdianne.gabriel@gmail.com>
2023-07-17 16:53:33 -0400

    Style aside for some flare

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <main className="w-full h-screen font-bodyFont bg-bodyBgColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
         <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-          <aside>
+          <aside className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
             <Socials />
           </aside>
         </div>
