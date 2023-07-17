5ddd81d2c784cdfbb0499b336c3143e2de91aa4d
Dianne <mdianne.gabriel@gmail.com>
2023-07-17 17:27:15 -0400

    Animate the side bars using framer motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,15 +16,23 @@ export default function Home() {
       <main className="w-full h-screen font-bodyFont bg-bodyBgColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
         <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-          <aside className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
+          <motion.aside
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0"
+          >
             <Socials />
-          </aside>
-          <section>
-            
-          </section>
-          <article className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0">
+          </motion.aside>
+          <section></section>
+          <motion.article
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
+          >
             <Email />
-          </article>
+          </motion.article>
         </div>
       </main>
     </>
