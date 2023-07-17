16d3e8a67fcb399c67dbfb257bb09479a1987d0d
Dianne <mdianne.gabriel@gmail.com>
2023-07-17 17:42:16 -0400

    Replace div with section. Style Introduction section

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Introduction = () => {
   return (
-    <div>Introduction</div>
+    <section
+      id="home"
+      className="max-w-contentContainer mx-auto py-10 mdL:py-24 flex flex-col gap-4 lgL:gap-8 mdL:px-10 xl:px-4"
+    >
+      Introduction
+    </section>
   );
 };
 
