8790be8aad9b417275302f94770b9bc6766d4f67
Dianne <mdianne.gabriel@gmail.com>
2023-07-17 17:08:19 -0400

    Style the email address

--- components/Email.tsx
+++ components/Email.tsx
@@ -4,7 +4,7 @@ const Email = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-end gap-6 text-textLight">
       <a href="mailto:mdianne.gabriel@gmail.com">
-        <p>mdianne.gabriel@gmail.com</p>
+        <p className="text-sm rotate-90 w-72 tracking-wide text-textAccent">mdianne.gabriel@gmail.com</p>
       </a>
     </div>
   );
