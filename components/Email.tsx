3ceb8c07f989f279f7c0c92399fb3054b2ee9af0
Dianne <mdianne.gabriel@gmail.com>
2023-07-17 17:05:37 -0400

    Add flex box to Email main div

--- components/Email.tsx
+++ components/Email.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Email = () => {
-  return <div></div>;
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-end gap-6 text-textLight">
+      asd
+    </div>
+  );
 };
 
 export default Email;
