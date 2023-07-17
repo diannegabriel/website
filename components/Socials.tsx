fb8b1c26309a10d9b43a9659c7c08d93ebaabb38
Dianne <mdianne.gabriel@gmail.com>
2023-07-17 16:46:16 -0400

    Add styling to github icon

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -6,7 +6,7 @@ const Socials = () => {
     <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
       <div className="flex flex-col gap-4">
         <a href="https://github.com/diannegabriel" target="_blank">
-          <span>
+          <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textAccent cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <GrGithub />
           </span>
         </a>
