d5eeea72bf16f240f41ea1a9bb724f339d0366b0
Dianne <mdianne.gabriel@gmail.com>
2023-07-17 16:41:37 -0400

    Modify html section elements

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -3,13 +3,15 @@ import { GrGithub } from "react-icons/gr";
 
 const Socials = () => {
   return (
-    <aside className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
-      <a href="https://github.com/diannegabriel" target="_blank">
-        <span>
-          <GrGithub />
-        </span>
-      </a>
-    </aside>
+    <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
+      <div className="flex flex-col gap-4">
+        <a href="https://github.com/diannegabriel" target="_blank">
+          <span>
+            <GrGithub />
+          </span>
+        </a>
+      </div>
+    </div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,9 @@ export default function Home() {
       <main className="w-full h-screen font-bodyFont bg-bodyBgColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
         <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-          <Socials />
+          <aside>
+            <Socials />
+          </aside>
         </div>
       </main>
     </>
