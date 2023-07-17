310bb5498585a763586cb4f3669d96fc0339a9e9
Dianne <mdianne.gabriel@gmail.com>
2023-07-17 16:50:17 -0400

    Add linkedin icon with style and hyperlink

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GrGithub } from "react-icons/gr";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const Socials = () => {
   return (
@@ -8,6 +9,11 @@ const Socials = () => {
         <a href="https://github.com/diannegabriel" target="_blank">
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textAccent cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <GrGithub />
+          </span>
+        </a>
+        <a href="https://www.linkedin.com/in/diannegabriel/" target="_blank">
+          <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textAccent cursor-pointer hover:-translate-y-2 transition-all duration-300">
+            <FaLinkedinIn />
           </span>
         </a>
       </div>
