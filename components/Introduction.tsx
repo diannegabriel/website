52f095bf5b3144f7d7856c5654d37eae0bbf1c32
Dianne <mdianne.gabriel@gmail.com>
2023-07-18 12:24:45 -0400

    Update introduction texts

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -21,9 +21,9 @@ const Introduction = () => {
         transition={{ duration: 0.5, delay: 0.7 }}
         className="text-4xl lgL:text-6xl font-titleFont font-semibold flex flex-col"
       >
-        Dianne Gabriel. <span className="text-textDark mt-2 lgL:mt-4">I make apps to help the world :)</span>
+        Dianne Gabriel. <span className="text-textDark mt-2 lgL:mt-4">Helping the world, one line at a time!</span>
       </motion.h1>
-      <p>I am a full stack developer with 2+ years of experience in the telecommunications industry.</p>
+      <p>With over 2 years of experience in full stack development, I bring a wealth of knowledge and expertise to the table. My keen eye for details ensures that every aspect of my work is crafted with precision and accuracy. I have a strong skill set in creating dynamic and user-friendly applications that meet the needs of modern users. Additionally, my proficiency in cloud systems allows me to seamlessly integrate scalable and efficient solutions. As a dedicated professional, I am committed to delivering high-quality results and contributing to the success of any project I undertake.</p>
     </section>
   );
 };
