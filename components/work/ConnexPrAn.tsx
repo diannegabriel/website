3fab9f16961a8a87c8a90a58de696f8f111efbe0
Dianne <mdianne.gabriel@gmail.com>
2023-07-20 09:16:18 -0400

    Lint ConnexPrAn

--- components/work/ConnexPrAn.tsx
+++ components/work/ConnexPrAn.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
 const ConnexPrAn = () => {
   return (
     <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ delay: 0.1 }}
-    className="w-full"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.1 }}
+      className="w-full"
     >
-      Connex Programmer Analyst
+      <h3>Programmer Analyst</h3>
     </motion.div>
-  )
-}
+  );
+};
 
-export default ConnexPrAn
+export default ConnexPrAn;
