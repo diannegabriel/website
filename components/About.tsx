930bd5d7a39e5fc867d5224a83f35391a4adda64
Dianne <mdianne.gabriel@gmail.com>
2023-07-19 21:31:15 -0400

    Style about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const About = () => {
-  return (<div>asd</div>);
+  return (
+    <section id="about" className="max-w-containerSmall mx-auto py-10 lgL:py-32 flex flex-col gap-8">
+      About
+    </section>
+    );
 };
 
 export default About
