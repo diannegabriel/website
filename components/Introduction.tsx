5e629c87e3da3e6c2c1d3cb8cc118267e78a02d1
Dianne <mdianne.gabriel@gmail.com>
2023-07-19 21:21:51 -0400

    Change padding and text width

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -5,7 +5,7 @@ const Introduction = () => {
   return (
     <section
       id="home"
-      className="max-w-contentContainer mx-auto py-10 mdL:py-12 flex flex-col gap-4 lgL:gap-8 mdL:px-10 xl:px-4"
+      className="max-w-contentContainer mx-auto py-10 mdL:py-16 flex flex-col gap-4 lgL:gap-8 mdL:px-10 xl:px-4"
     >
       <motion.h3
         initial={{ y: 10, opacity: 0 }}
@@ -30,7 +30,7 @@ const Introduction = () => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.8 }}
-        className="text-base md:max-w-[650px] text-textDark font-medium"
+        className="text-base md:max-w-[800px] text-textDark font-medium"
       >
         I've been into full-stack development for over 2 awesome years,
         and I've got quite a bag of tricks up my sleeve! My keen eye for details
@@ -42,7 +42,7 @@ const Introduction = () => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.8 }}
-        className="text-base md:max-w-[650px] text-textDark font-medium"
+        className="text-base md:max-w-[800px] text-textDark font-medium"
       >        Creating dynamic and user-friendly applications is kind of my jam – I
         love meeting the needs of modern users and making their experience a
         breeze. Plus, I'm pretty handy with cloud systems, so integrating
@@ -53,7 +53,7 @@ const Introduction = () => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.8 }}
-        className="text-base md:max-w-[650px] text-textDark font-medium"
+        className="text-base md:max-w-[800px] text-textDark font-medium"
       >        When it comes to work, I'm all in! As a dedicated professional, I'm
         committed to delivering top-notch results and doing my part to make each
         project a smashing success.
@@ -63,7 +63,7 @@ const Introduction = () => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.8 }}
-        className="text-base md:max-w-[650px] text-textDark font-medium"
+        className="text-base md:max-w-[800px] text-textDark font-medium"
       >        Let's create something amazing together! 🚀
       </motion.p>
     </section>
