6cf5445b3d62e64b52114233f9fa90e714e07885
Dianne <mdianne.gabriel@gmail.com>
2023-07-20 09:07:58 -0400

    Add more to list

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -11,7 +11,10 @@ const Experience = () => {
         <span className="hidden md:inline-flex md:w-60 lgL:w-72 h-[0.5px] bg-gray-700 ml-6"></span>
       </h2>
       <div className="w-full mt-10 flex flex-col md:flex-row gap-16">
-        <ul>
+        <ul className="md:w-32 flex flex-col">
+          <li className="border-l-2 border-l-textAccent text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">Connex</li>
+          <li className="border-l-2 border-l-textAccent text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">Connex</li>
+          <li className="border-l-2 border-l-textAccent text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">Connex</li>
           <li className="border-l-2 border-l-textAccent text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">Connex</li>
         </ul>
       </div>
