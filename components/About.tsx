89862325b9df7249953b9c1b00b5a91f301832b5
Dianne <mdianne.gabriel@gmail.com>
2023-07-20 06:54:11 -0400

    Stylize about me text

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section
       id="about"
-      className="max-w-containerSmall mx-auto py-10 lgL:py-32 flex flex-col gap-8"
+      className="max-w-containerSmall mx-auto py-10 lgL:py-28 flex flex-col gap-8"
     >
       <h2 className="font-titleFont text-2xl font-semibold flex items-center">
         About Me
@@ -15,12 +15,21 @@ const About = () => {
           <p>
             I got into the world of tech and programming languages as early as
             10 years old. I clearly remember coding my very first HTML, CSS, and
-            JavaScript project and immediately got hooked! I enjoyed the
-            satisfaction of creating something from scratch and having the
-            creative freedom over it. 💻 While tech wasn't my first choice as a
-            career after high school, my passion and love for coding has always
-            lived within me. Finally, after deciding to switch careers, I
-            enrolled to Lighthouse Labs Full Stack Web Dev Bootcamp in 2021. ✨
+            JavaScript project and immediately got hooked (had to go through
+            hoops to integrate Friendster custom profiles, but my favourite
+            thing to work on were MySpace profiles)!
+          </p>
+
+          <p>
+            I enjoyed the satisfaction of creating something from scratch and
+            having the creative freedom over it. While tech wasn't my first
+            choice as a career after high school, my passion and love for coding
+            has always lived within me. Finally, after deciding to switch
+            careers, I enrolled to Lighthouse Labs Full Stack Web Development Bootcamp
+            in 2021.
+          </p>
+
+          <p>
             After finishing the extremely challenging bootcamp, I realized that
             I truly enjoy the process of creating apps and products that solve
             people's problems. I strive to make self-care, motivational, and
