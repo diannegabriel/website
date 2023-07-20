fc3b3d5b3e30e419380bc537656582f3c0bd5096
Dianne <mdianne.gabriel@gmail.com>
2023-07-20 08:34:57 -0400

    Lint experience component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Experience = () => {
   return (
-    <section id="experience" className="max-w-containerXS mx-auto py-10 lgL:py-24 px-4">
+    <section
+      id="experience"
+      className="max-w-containerXS mx-auto py-10 lgL:py-24 px-4"
+    >
       <h2 className="font-titleFont text-2xl font-semibold flex items-center">
         Experience
         <span className="hidden md:inline-flex md:w-60 lgL:w-72 h-[0.5px] bg-gray-700 ml-6"></span>
       </h2>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
