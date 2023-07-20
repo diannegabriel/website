d3c5adc457873cded9c2f62f12b0b854f50cd9cb
Dianne <mdianne.gabriel@gmail.com>
2023-07-20 09:54:58 -0400

    Apply handles to onClick events

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -26,10 +26,16 @@ const Experience = () => {
       </h2>
       <div className="w-full mt-10 flex flex-col md:flex-row gap-16">
         <ul className="md:w-32 flex flex-col">
-          <li className="border-l-2 border-l-textAccent text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">
+          <li
+            onClick={handleConnexPrAn}
+            className="border-l-2 border-l-textAccent text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium"
+          >
             Connex
           </li>
-          <li className="border-l-2 border-l-hoverColor text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">
+          <li
+            onClick={handleConnexJr}
+            className="border-l-2 border-l-hoverColor text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium"
+          >
             Connex
           </li>
           <li className="border-l-2 border-l-hoverColor text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">
