0e60f330446ff225078e22ecd4888d4b62115ca0
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 17:20:49 -0400

    Apply onlick for hamburger icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -86,7 +86,10 @@ const Navbar = () => {
           </a>
         </div>
         {/* Hamburger icon */}
-        <div className="w-6 h-5 flex flex-col justify-between items-center mdL:hidden text-4xl text-textAccent cursor-pointer overflow-hidden group">
+        <div
+          onClick={() => setToggleMenu(true)}
+          className="w-6 h-5 flex flex-col justify-between items-center mdL:hidden text-4xl text-textAccent cursor-pointer overflow-hidden group"
+        >
           <span className="w-full h-[2px] bg-textAccent inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
           <span className="w-full h-[2px] bg-textAccent inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300"></span>
           <span className="w-full h-[2px] bg-textAccent inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300"></span>
