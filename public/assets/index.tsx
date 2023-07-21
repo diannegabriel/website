e9a69c8d5bb55148c1e6d936700a225944b9ac89
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 14:16:05 -0400

    Export hypehub

--- public/assets/index.tsx
+++ public/assets/index.tsx
@@ -3,5 +3,6 @@ import logoGif from "./images/logo.gif";
 import dianneGif from "./images/dianne.gif";
 import dianneJpg from "./images/dianne.jpg";
 import motivadid from "./images/motivadid.png";
+import hypehub from "./images/hypehub.png";
 
-export { logoPng, logoGif, dianneGif, dianneJpg, motivadid };
+export { logoPng, logoGif, dianneGif, dianneJpg, motivadid, hypehub };
