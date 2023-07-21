8440c8a3898d674e4162fda11be2a2a6fda280b8
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 14:51:10 -0400

    Disable aside elements

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,28 +20,28 @@ export default function Home() {
       <main className="w-full h-screen font-bodyFont bg-bodyBgColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
         <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-          <motion.aside
+          {/* <motion.aside
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1.5 }}
             className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0"
           >
             <Socials />
-          </motion.aside>
+          </motion.aside> */}
           <article className="h-[88vh] w-full mx-auto p-4">
             <Introduction />
             <About />
             <Experience />
             <Projects />
           </article>
-          <motion.aside
+          {/* <motion.aside
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1.5 }}
             className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
           >
             <Email />
-          </motion.aside>
+          </motion.aside> */}
         </div>
       </main>
     </>
