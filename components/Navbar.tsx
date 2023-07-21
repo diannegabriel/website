64309d12e1506e399d6efe7053cba0330d10fe43
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 17:32:11 -0400

    Import MdClose

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
+import { MdClose } from "react-icons/md";
 
 const Navbar = () => {
   const ref = useRef<string | any>("");
