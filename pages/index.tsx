745efc05e1e978bdbec29ef28b368b327336c850
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 17:09:43 -0400

    Stylize scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Head>
         <title>Dianne's Portfolio</title>
       </Head>
-      <main className="w-full h-screen font-bodyFont bg-bodyBgColor text-textLight overflow-x-hidden overflow-y-scroll scroll-smooth">
+      <main className="w-full h-screen font-bodyFont bg-bodyBgColor text-textLight overflow-x-hidden overflow-y-scroll scroll-smooth scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
         <Navbar />
         <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
           {/* <motion.aside
