5cfbd3120764e017b35b8e437ce9389dfbbb71b9
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 12:11:08 -0400

    Import motivadid screenshot

--- public/assets/index.tsx
+++ public/assets/index.tsx
@@ -2,5 +2,6 @@ import logoPng from "./images/logo.png";
 import logoGif from "./images/logo.gif";
 import dianneGif from "./images/dianne.gif";
 import dianneJpg from "./images/dianne.jpg";
+import motivadid from "./images/motivadid.png";
 
-export { logoPng, logoGif, dianneGif, dianneJpg };
+export { logoPng, logoGif, dianneGif, dianneJpg, motivadid };
