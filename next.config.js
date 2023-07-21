cb863e8f0a4a59710e0fb0f1bafc10d8d07714e1
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 19:42:58 -0400

    Set webpack to false

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-}
+  reactStrictMode: true,
+  // future: { webpack5: true }
+  webpack: false
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
