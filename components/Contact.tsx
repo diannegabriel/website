77a0b9f1e732866177ae5e6db235a5e442d538e6
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 15:29:49 -0400

    Import react icons for socials

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { GrGithub } from "react-icons/gr";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <section
       id="contact"
-      className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
+      className="max-w-contentContainer mx-auto py-10 xl:py-64 flex flex-col gap-4 items-center justify-center"
     >
-      <p className="font-titleFont text-lg text-textAccent font-semibold flex items-center tracking-wide">
+      {/* <p className="font-titleFont text-lg text-textAccent font-semibold flex items-center tracking-wide">
         Contact
-      </p>
+      </p> */}
       <h2 className="font-titleFont text-5xl font-semibold">Let's connect!</h2>
       <p className="max-w-[600px] text-center text-textDark">
       I'm currently on the hunt for some exciting new opportunities in the tech world! Whether it's building awesome web apps, managing cloud systems, or diving into a completely new territory, I'm ready to bring my skills and passion to a team that's all about making cool things happen. Let's collaborate and create some tech magic together! 
