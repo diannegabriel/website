e3056101b216fff757adc0d7c901cefc8d684cf7
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 17:15:38 -0400

    Import useState for menu toggling for small screen

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { dianneGif } from "@/public/assets";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyBgColor px-4">
       <div className="max-w-container h-full mx-auto py-1 font-titleFont font-medium flex items-center justify-between">
