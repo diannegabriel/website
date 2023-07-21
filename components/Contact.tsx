29ef244bab1d16249e8c049e08f6812272445ebe
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 16:07:47 -0400

    Add email to contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { GrGithub } from "react-icons/gr";
 import { FaLinkedinIn } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Contact = () => {
   return (
@@ -30,6 +31,11 @@ const Contact = () => {
         <a href="https://www.linkedin.com/in/diannegabriel/" target="_blank">
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textAccent cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <FaLinkedinIn />
+          </span>
+        </a>
+        <a href="mailto:mdianne.gabriel@gmail.com">
+          <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textAccent cursor-pointer hover:-translate-y-2 transition-all duration-300">
+            <MdEmail />
           </span>
         </a>
       </div>
