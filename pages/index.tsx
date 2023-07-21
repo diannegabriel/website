85c4f4b18fe69e1fc414ec1caa5d6bff82694cc4
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 16:27:07 -0400

    Disable footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 // const inter = Inter({ subsets: ['latin'] })
 
