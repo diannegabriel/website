b7bb5c5374a68233403859a719605a3637727480
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 15:28:26 -0400

    Add flavour text about looking for a job

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,6 +9,10 @@ const Contact = () => {
       <p className="font-titleFont text-lg text-textAccent font-semibold flex items-center tracking-wide">
         Contact
       </p>
+      <h2 className="font-titleFont text-5xl font-semibold">Let's connect!</h2>
+      <p className="max-w-[600px] text-center text-textDark">
+      I'm currently on the hunt for some exciting new opportunities in the tech world! Whether it's building awesome web apps, managing cloud systems, or diving into a completely new territory, I'm ready to bring my skills and passion to a team that's all about making cool things happen. Let's collaborate and create some tech magic together! 
+      </p>
     </section>
   );
 };
