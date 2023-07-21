24f0527b722c636f921d00366d207660c06cfb87
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 16:56:10 -0400

    Adjust spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="hidden mdL:inline-flex items-center justify-center w-full gap-4 lgL:py-36 text-textDark text-sm">
+    <footer className="italic hidden mdL:inline-flex items-center justify-center w-full gap-4 lgL:py-8 text-textDark text-sm">
       <p>
         Coded in Visual Studio Code. Built with Next.js and Tailwind CSS.
-        Deployed with Vercel.
+        Deployed with Vercel. Design inspired by <a href="https://v4.brittanychiang.com/">Brittany Chiang</a>.
       </p>
     </footer>
   );
