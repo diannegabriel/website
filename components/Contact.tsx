b9e795f91d8233004b3dbbcd81164fe2a40c98af
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 16:05:33 -0400

    Add socials to the bottom of contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,11 +6,12 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="max-w-contentContainer mx-auto py-10 lg:py-72 flex flex-col gap-4 items-center justify-center"
+      className="max-w-contentContainer mx-auto py-10 lg:py-72 flex flex-col gap-8 items-center justify-center"
     >
       {/* <p className="font-titleFont text-lg text-textAccent font-semibold flex items-center tracking-wide">
         Contact
       </p> */}
+      <div></div>
       <h2 className="font-titleFont text-5xl font-semibold">Let's connect!</h2>
       <p className="max-w-[600px] text-center text-textDark">
         I'm currently on the hunt for some exciting new opportunities in the
@@ -19,13 +20,26 @@ const Contact = () => {
         my skills and passion to a team that's all about making cool things
         happen. Let's collaborate and create some tech magic together!
       </p>
-      <div className="mt-6 flex items-center justify-center">
+      <span className="hidden md:inline-flex md:w-60 lgL:w-72 h-[0.5px] bg-gray-700"></span>
+      <div className="flex gap-6">
+        <a href="https://github.com/diannegabriel" target="_blank">
+          <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textAccent cursor-pointer hover:-translate-y-2 transition-all duration-300">
+            <GrGithub />
+          </span>
+        </a>
+        <a href="https://www.linkedin.com/in/diannegabriel/" target="_blank">
+          <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textAccent cursor-pointer hover:-translate-y-2 transition-all duration-300">
+            <FaLinkedinIn />
+          </span>
+        </a>
+      </div>
+      {/* <div className="mt-6 flex items-center justify-center">
         <a href="/assets/Dianne_Gabriel_Resume.pdf" target="_blank">
           <button className="w-36 h-12 rounded-md text-textAccent text-[15px] border border-textAccent hover:bg-hoverColor duration-300">
             Expand
           </button>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
