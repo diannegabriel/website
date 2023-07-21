38874546142fe86ebb4721bc7c207b53e1ae3678
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 14:11:30 -0400

    Add new project Hypehub

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -50,6 +50,41 @@ const Projects = () => {
             </div>
           </div>
         </div>
+        <div className="flex flex-col xl:flex-row-reverse gap-6">
+          <a
+            className="w-full xl:w-1/2 h-auto relative group"
+            href="https://diannegabriel.github.io/todo-list/"
+            target="_blank"
+          >
+            <div>
+              <Image
+                className="w-full h-full object-contain"
+                src={motivadid}
+                alt="MotivaDid App"
+              />
+            </div>
+          </a>
+          <div className="w-full xl:w-1/2 flex flex-col gap-6 lgL:justify-between items-end text-right z-10">
+            <p className="font-titleFont text-textAccent text-sm tracking-wide">
+              Featured Project
+            </p>
+            <h3 className="text-2xl font-bold">HypeHub</h3>
+            <p className="bg-[#262452] text-sm md:text-base xl:-mr-16 xl:ml-16 p-2 md:p-6 rounded-md">
+            HypeHUB is the ultimate task manager with a gaming twist – it's like your personal cheering squad, keeping you pumped and motivated while you crush your goals and level up your self-improvement game.
+            </p>
+            <ul className="flex justify-between md:gap-5 sm:gap-3 font-titleFont md:text-sm sm:text-xs text-textDark tracking-wide">
+              <li>JavaScript</li>
+              <li>React.js</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Local Storage</li>
+            </ul>
+            <div className="text-2xl flex gap-4">
+              <a href="https://github.com/diannegabriel/todo-list" target="_blank"><GrGithub /></a>
+              <a href="https://diannegabriel.github.io/todo-list/" target="_blank"><BsBrowserChrome /></a>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
