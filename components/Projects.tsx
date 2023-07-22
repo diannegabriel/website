3fb7465f9c8db760f930396ffd26c550394ab4e4
Dianne <mdianne.gabriel@gmail.com>
2023-07-21 21:23:33 -0400

    Separate projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
 import React from "react";
-import { hypehub, motivadid } from "@/public/assets";
-import { GrGithub } from "react-icons/gr";
-import { BsBrowserChrome } from "react-icons/bs";
-import { BiSolidVideos } from "react-icons/bi";
+import HypeHub from "./projects/HypeHub";
+import MotivaDid from "./projects/MotivaDid";
 
 const Projects = () => {
   return (
@@ -16,115 +13,15 @@ const Projects = () => {
         <span className="hidden md:inline-flex md:w-60 lgL:w-72 h-[0.5px] bg-gray-700 ml-6"></span>
       </h2>
       <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
-        <div className="flex flex-col xl:flex-row gap-6">
-          <a
-            className="w-full xl:w-1/2 h-auto relative group"
-            href="https://diannegabriel.github.io/todo-list/"
-            target="_blank"
-          >
-            <div>
-              <Image
-                className="w-full h-full object-contain"
-                src={motivadid}
-                alt="MotivaDid App"
-              />
-            </div>
-          </a>
-          <div className="w-full xl:w-1/2 flex flex-col gap-6 lgL:justify-between items-end text-right xl:-ml-16 z-10">
-            <div>
-              <p className="font-titleFont text-textAccent text-sm tracking-wide">
-                Featured Project
-              </p>
-              <h3 className="text-2xl font-bold">MotivaDid</h3>
-            </div>
-            <p className="bg-[#262452] text-sm md:text-base p-2 md:p-6 rounded-md">
-              A super cool todo web app that not only helps you get stuff done
-              but also sprinkles in motivational quotes as you tackle your
-              tasks. It's like a double shot of productivity and inspiration in
-              one sweet package!
-            </p>
-            <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-              <li>JavaScript</li>
-              <li>React.js</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Local Storage</li>
-            </ul>
-            <div className="text-2xl flex gap-4">
-              <a
-                href="https://github.com/diannegabriel/todo-list"
-                target="_blank"
-              >
-                <GrGithub />
-              </a>
-              <a
-                href="https://diannegabriel.github.io/todo-list/"
-                target="_blank"
-              >
-                <BsBrowserChrome />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col xl:flex-row-reverse gap-6">
-          <a
-            className="w-full xl:w-1/2 h-auto relative group"
-            href="https://github.com/diannegabriel/HypeHUB"
-            target="_blank"
-          >
-            <div>
-              <Image
-                className="w-full h-full object-contain"
-                src={hypehub}
-                alt="MotivaDid App"
-              />
-            </div>
-          </a>
-          <div className="w-full xl:w-1/2 flex flex-col gap-6 lgL:justify-between items-end text-right z-10">
-            <div>
-              <p className="font-titleFont text-textAccent text-sm tracking-wide">
-                Featured Project
-              </p>
-              <h3 className="text-2xl font-bold">HypeHUB</h3>
-            </div>
-            <p className="bg-[#262452] text-sm md:text-base xl:-mr-16 xl:ml-16 p-2 md:p-6 rounded-md">
-              HypeHUB is the ultimate task manager with a gaming twist – it's
-              like your personal cheering squad, keeping you pumped and
-              motivated while you crush your goals and level up your
-              self-improvement game.
-            </p>
-            <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-              <li>JavaScript</li>
-              <li>React.js</li>
-              <li>Bootstrap</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>MongoDB</li>
-              <li>Spotify API</li>
-            </ul>
-            <div className="text-2xl flex gap-4">
-              <a
-                href="https://github.com/diannegabriel/HypeHUB"
-                target="_blank"
-              >
-                <GrGithub />
-              </a>
-              <a
-                href="https://www.youtube.com/watch?v=uFkGQgNHRfQ"
-                target="_blank"
-              >
-                <BiSolidVideos />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center justify-center">
-          <a href="https://github.com/diannegabriel" target="_blank">
-            <button className="w-36 h-12 rounded-md text-textAccent text-[15px] border border-textAccent hover:bg-hoverColor duration-300">
-              View more
-            </button>
-          </a>
-        </div>
+        <MotivaDid />
+        <HypeHub />
+      </div>
+      <div className="pt-36 flex items-center justify-center">
+        <a href="https://github.com/diannegabriel?tab=repositories" target="_blank">
+          <button className="w-36 h-12 rounded-md text-textAccent text-[15px] border border-textAccent hover:bg-hoverColor duration-300">
+            View more
+          </button>
+        </a>
       </div>
     </section>
   );
--- components/projects/HypeHub.tsx
+++ components/projects/HypeHub.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { hypehub } from "@/public/assets";
+import { GrGithub } from "react-icons/gr";
+import { BiSolidVideos } from "react-icons/bi";
 
 const HypeHub = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col xl:flex-row-reverse gap-6">
+      <a
+        className="w-full xl:w-1/2 h-auto relative group"
+        href="https://github.com/diannegabriel/HypeHUB"
+        target="_blank"
+      >
+        <div>
+          <Image
+            className="w-full h-full object-contain"
+            src={hypehub}
+            alt="MotivaDid App"
+          />
+        </div>
+      </a>
+      <div className="w-full xl:w-1/2 flex flex-col gap-6 lgL:justify-between items-end text-right z-10">
+        <div>
+          <p className="font-titleFont text-textAccent text-sm tracking-wide">
+            Featured Project | Group
+          </p>
+          <h3 className="text-2xl font-bold">HypeHUB</h3>
+        </div>
+        <p className="bg-[#262452] text-sm md:text-base xl:-mr-16 xl:ml-16 p-2 md:p-6 rounded-md">
+          HypeHUB is the ultimate task manager with a gaming twist – it's like
+          your personal cheering squad, keeping you pumped and motivated while
+          you crush your goals and level up your self-improvement game.
+        </p>
+        <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+          <li>JavaScript</li>
+          <li>React.js</li>
+          <li>Bootstrap</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>MongoDB</li>
+          <li>Spotify API</li>
+        </ul>
+        <div className="text-2xl flex gap-4">
+          <a href="https://github.com/diannegabriel/HypeHUB" target="_blank">
+            <GrGithub />
+          </a>
+          <a href="https://www.youtube.com/watch?v=uFkGQgNHRfQ" target="_blank">
+            <BiSolidVideos />
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default HypeHub;
