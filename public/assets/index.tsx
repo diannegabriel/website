3f7a66c4a195daf7da6656f5d1295f504e86ef4c
Dianne <mdianne.gabriel@gmail.com>
2023-07-27 18:47:32 -0400

    Upload new version of Motivadid

--- public/assets/index.tsx
+++ public/assets/index.tsx
@@ -2,7 +2,7 @@ import logoPng from "./images/logo.png";
 import logoGif from "./images/logo.gif";
 import dianneGif from "./images/dianne.gif";
 import dianneJpg from "./images/dianne.jpg";
-import motivadid from "./images/motivadid.png";
+import motivadid from "./images/motivadid_ver2.png";
 import hypehub from "./images/hypehub.png";
 
 export { logoPng, logoGif, dianneGif, dianneJpg, motivadid, hypehub };
