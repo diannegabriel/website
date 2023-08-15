06265bcfc88a82a6ab65be958908ea2e0c417d5e
Dianne <mdianne.gabriel@gmail.com>
2023-08-14 20:27:00 -0400

    Apply styling to blabbybluejay

--- components/projects/BlabbyBlueJay.tsx
+++ components/projects/BlabbyBlueJay.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import { blabbybluejay } from "@/public/assets";
 import { GrGithub } from "react-icons/gr";
@@ -6,10 +6,51 @@ import { BsBrowserChrome } from "react-icons/bs";
 
 const BlabbyBlueJay = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col xl:flex-row gap-6">
+      <a
+        className="w-full xl:w-1/2 h-auto relative group"
+        href="https://diannegabriel.github.io/todo-list/"
+        target="_blank"
+      >
+        <div>
+          <Image
+            className="w-full h-full object-contain"
+            src={blabbybluejay}
+            alt="MotivaDid App"
+          />
+        </div>
+      </a>
+      <div className="w-full xl:w-1/2 flex flex-col gap-6 lgL:justify-between items-end text-right xl:-ml-16 z-10">
+        <div>
+          <p className="font-titleFont text-textAccent text-sm tracking-wide">
+            Featured Project
+          </p>
+          <h3 className="text-2xl font-bold">MotivaDid</h3>
+        </div>
+        <p className="bg-[#262452] text-sm md:text-base p-2 md:p-6 rounded-md">
+          A super cool todo web app that not only helps you get stuff done but
+          also sprinkles in motivational quotes as you tackle your tasks. It's
+          like a double shot of productivity and inspiration in one sweet
+          package!
+        </p>
+        <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+          <li>JavaScript</li>
+          <li>React.js</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Local Storage</li>
+        </ul>
+        <div className="text-2xl flex gap-4">
+          <a href="https://github.com/diannegabriel/todo-list" target="_blank">
+            <GrGithub />
+          </a>
+          <a href="https://diannegabriel.github.io/todo-list/" target="_blank">
+            <BsBrowserChrome />
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlabbyBlueJay
+export default BlabbyBlueJay;
