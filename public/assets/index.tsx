82c45aa8b8fcacda1be93880ca8ed9bdb812ea23
Dianne <mdianne.gabriel@gmail.com>
2023-08-21 09:54:59 -0400

    Add more projects

--- public/assets/index.tsx
+++ public/assets/index.tsx
@@ -5,5 +5,7 @@ import dianneJpg from "./images/dianne.jpg";
 import motivadid from "./images/motivadid_ver2.png";
 import hypehub from "./images/hypehub.png";
 import blabbybluejay from "./images/blabbybluejay.png"
+import tinyapp from "./images/tinyapp.png"
+import tweeter from "./images/tweeter.png"
 
-export { logoPng, logoGif, dianneGif, dianneJpg, motivadid, hypehub, blabbybluejay };
+export { logoPng, logoGif, dianneGif, dianneJpg, motivadid, hypehub, blabbybluejay, tinyapp, tweeter };
